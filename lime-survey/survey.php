edbaf7e266aeb5e96b5dbc0bf4744f292bd6b4a9
Raul Suarez <rsuarez@zipdata.net>
2023-05-10 16:49:51 -0400

    Modified

--- lime-survey/survey.php
+++ lime-survey/survey.php
@@ -1,19 +1,11 @@
 <?php
 
 /**
- * FilesystemStorage plugin
- *
- * Allows attachment data to be written to the disk rather than in the
- * database
  */
 require_once(INCLUDE_DIR.'class.plugin.php');
 require_once('config.php');
 
 class LimeSurveyPlugin extends Plugin {
-    var $config_class = 'LimeSurveyConfig';
-
-    function bootstrap() {
-        $config = $this->getConfig();
-    }
+    LimeSurveyConfig()
 }
 
