bd87042dd9c7cb565e36ee6692ea1305b9a534da
Help Desk System for WELS <helpdesk@cairo.usa.pc-depot.com>
2023-05-11 12:14:01 -0400

    Added basic HTTP Request to check the settings of the pluggin. Added some validators for the username, password and SurveyID.

--- lime-survey/survey.php
+++ lime-survey/survey.php
@@ -5,11 +5,8 @@
 require_once(INCLUDE_DIR.'class.plugin.php');
 require_once('config.php');
 
-use jsonRPCphp/org/jsonrpcphp/JsonRPCClient.php;
-
 class LimeSurveyPlugin extends Plugin {
     var $config_class = 'LimeSurveyConfig';
 
 
 }
-
