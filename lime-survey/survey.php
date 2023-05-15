3bd80c0136a122216f50b848ec7c5884e7614ecd
Help Desk System for WELS <helpdesk@cairo.zipdata.net>
2023-05-15 18:43:32 -0400

    Trusted method. Finally working the trigger

--- lime-survey/survey.php
+++ lime-survey/survey.php
@@ -1,62 +1,30 @@
 <?php
 
-require_once(INCLUDE_DIR . 'class.plugin.php');
-require_once(INCLUDE_DIR.'/class.ticket.php');
+require_once (INCLUDE_DIR . 'class.plugin.php');
+require_once (INCLUDE_DIR . 'class.signal.php');
+require_once (INCLUDE_DIR . 'class.app.php');
+require_once (INCLUDE_DIR . 'class.dispatcher.php');
+require_once (INCLUDE_DIR . 'class.dynamic_forms.php');
+require_once (INCLUDE_DIR . 'class.osticket.php');
+require_once(INCLUDE_DIR . 'class.ticket.php');
 require_once('config.php');
 
-// class EnrollInSurvey extends Ticket {
-//     /**
-//      * Registers a callback function for the ticket.closed event
-//      */
-//     function registerCallback() {
-//         Ticket::setStatus( 'closed', function() {
-//             // Retrieve the LimeSurvey server settings from configuration
-//             $config = Plugin::getConfig('limesurvey');
-//             $server = $config->getServerSettings();
+class EnrollInSurvey extends Ticket {
 
-//             // Create a JsonRPCClient object to connect to LimeSurvey API
-//             $client = new JsonRPCClient($server['domain'].'/index.php/admin/remotecontrol');
-//             $sessionKey = $client->get_session_key($server['user'], $server['passwd']);
-
-//             // Retrieve the survey ID from configuration
-//             $surveyId = $config->getSurveyID();
-
-//             // Add the ticket requester as a participant in the survey
-//             // $participants = array(
-//             //     'email' => $ticket->getEmail(),
-//             //     'firstname' => $ticket->getName(),
-//             //     'lastname' => 'Second'
-//             // );
-//             $participants = array(
-//                 "email"=>"raulnsuarez@gmail.com",
-//                 "lastname"=>"Suarez",
-//                 "firstname"=>"Raul"
-//             );
-//             $result = $client->add_participants($sessionKey, $surveyId, array($participants));
-//             if ($result === null) {
-//                 // An error occurred while adding the participant
-//                 $ticket->logError('Failed to enroll ticket requester in LimeSurvey');
-//             }else{
-//                 $ticket->logError('Successfully enrolled ticket requester in LimeSurvey');
-//             }
-
-//             // Release the LimeSurvey API session key
-//             $client->release_session_key($sessionKey);
-//         } );
-//     }
-// }
+}
 
 class LimeSurveyPlugin extends Plugin {
     var $config_class = 'LimeSurveyConfig';
 
     function bootstrap() {
-        //..
+        $config = $this->getConfig();
+        $server = $config->getServerSettings();
+        Signal::connect('ticket.created', function($ticket){ 
+            $this->enrrollTicketRequesterInLimeSurvey($server);
+        });
     }
     
-    public function enrrollTicketRequesterInLimeSurvey($ticket) {
-        // Get the LimeSurvey API endpoint URL and API key from your configuration
-        $server = $this->getConfig()->getServerSettings();
-        
+    function enrrollTicketRequesterInLimeSurvey($server) {       
         // Create a JsonRPCClient object to connect to LimeSurvey API
         $client = new JsonRPCClient($server['domain'].'/index.php/admin/remotecontrol');
         $sessionKey = $client->get_session_key($server['user'], $server['passwd']);
@@ -78,11 +46,11 @@ class LimeSurveyPlugin extends Plugin {
         $result = $client->add_participants($sessionKey, $surveyId, array($participants));
         if ($result === null) {
             // An error occurred while adding the participant
-            $ticket->logError('Failed to enroll ticket requester in LimeSurvey');
+            //$ticket->logError('Failed to enroll ticket requester in LimeSurvey');
         }else{
             // Save the survey response ID in the ticket metadata
-            $ticket->addNote("LimeSurvey response ID: $result");
-            $ticket->logError('Successfully enrolled ticket requester in LimeSurvey');
+            //$ticket->addNote("LimeSurvey response ID: $result");
+            //$ticket->logError('Successfully enrolled ticket requester in LimeSurvey');
         }
 
         // Release the LimeSurvey API session key
